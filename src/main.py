d0f95d7f61141a0b2e42b97f60833fd1c3c4a42d
Hugo Duarte <hduarte@MacBook-Air-de-Hugo.local>
2026-04-18 16:36:52 +0400

    Add basic executable script for testing

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Project running correctly")
+import sys
+
+def main():
+    print("Python version:", sys.version)
+    print("Project setup is working correctly")
+
+if __name__ == "__main__":
+    main()
